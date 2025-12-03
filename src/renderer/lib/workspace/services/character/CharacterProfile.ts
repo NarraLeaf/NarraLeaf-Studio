c1825fde403853dfc028a6d6b3ecb813dbab5303
Nomen <111544765+helloyork@users.noreply.github.com>
2025-12-02 17:43:04 -0800

    update: implement character appearance management with variants and groups

--- src/renderer/lib/workspace/services/character/CharacterProfile.ts
+++ src/renderer/lib/workspace/services/character/CharacterProfile.ts
@@ -1,5 +1,20 @@
+import { CharacterAppearance } from "./CharacterAppearance";
+import { CharacterEditorProfile, ICharacterAppearance } from "./types";
 
-
+export interface CharacterProfileConfig extends CharacterEditorProfile {
+    appearance: ICharacterAppearance;
+}
 
 export class CharacterProfile {
+    public readonly appearance: CharacterAppearance;
+    private readonly profile: CharacterEditorProfile;
+
+    constructor(config: CharacterProfileConfig) {
+        this.profile = config;
+        this.appearance = new CharacterAppearance(config.appearance);
+    }
+
+    public getProfile(): CharacterEditorProfile {
+        return this.profile;
+    }
 }
--- src/renderer/lib/workspace/services/core/CharacterService.ts
+++ src/renderer/lib/workspace/services/core/CharacterService.ts
@@ -1,10 +1,12 @@
 import { Service } from "../Service";
 import { Services, WorkspaceContext } from "../services";
+import { AssetsService } from "./AssetsService";
 import { FileSystemService } from "./FileSystem";
 
 export class CharacterService extends Service<CharacterService> implements CharacterService {
     protected async init(ctx: WorkspaceContext, depend: (services: Service[]) => Promise<void>): Promise<void> {
         const filesystemService = ctx.services.get<FileSystemService>(Services.FileSystem);
-        await depend([filesystemService]);
+        const assetsService = ctx.services.get<AssetsService>(Services.Assets);
+        await depend([filesystemService, assetsService]);
     }
 }
